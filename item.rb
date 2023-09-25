15ddb2a3f61dca853d6324908f606b69b0b48ae1
MasumaJaffery <masumajaffery1@gmail.com>
2023-09-25 15:44:39 -0700

    item.rb

---
+++ item.rb
@@ -0,0 +1,23 @@
+require 'date'
+
+class Item
+ attr_accessor :label, :author, :genre, :date
+ attr_reader :id, :archived
+ def initialize(label, author, genre, publish_date)
+    @label = label,
+    @author = author,
+    @genre = genre,
+    @publish_date = Date.parse(publish_date),
+    @id: Random.rand(1...1000),
+    @archived = false
+ end
+
+ def can_be_archived?
+
+ end
+
+ def move_to_archive
+
+ end
+
+end
