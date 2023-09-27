dbe9c4fc835d881ee5aec0a4647829497c0f4f62
Najibullah Jafari <121656832+najibullahjafari@users.noreply.github.com>
2023-09-27 08:33:41 +0430

    update

--- lib/main.rb
+++ lib/main.rb
@@ -1,50 +1,53 @@
 def main
-  loop 
-   puts " 1- List all books"
-    puts "2 - List all music albums"
-    puts "3 - List all movies"
-    puts "4 - List of games"
+  puts 'Welcome to our Catalog App'
+  loop do # Fixed the loop do syntax
+    puts '1 - List all books'
+    puts '2 - List all music albums'
+    puts '3 - List all movies'
+    puts '4 - List of games'
     puts "5 - List all genres (e.g 'Comedy', 'Thriller')"
     puts "6 - List all labels (e.g. 'Gift', 'New')"
     puts "7 - List all authors (e.g. 'Stephen King')"
     puts "8 - List all sources (e.g. 'From a friend', 'Online shop')"
-    puts "9 -  Add a book"
-    puts "10 - Add a music album"
-    puts "11 - Add a movie"
-    puts "12 - Add a game"
-    puts "13 - Exit"
-  end
-  choice = gets.chomp.to_i 
-when choice
-    case 1
-        #run a the list of books
-    case 2
-
-    case 3
-
-    case 4
-
-    case 5
-
-    case 6
-
-    case 7
-
-    case 8
-
-    case 9
-
-    case 10
-
-    case 11
-
-    case 12
-
-    case 13
-        
-    else  
-        puts "Invalide choice"
+    puts '9 -  Add a book'
+    puts '10 - Add a music album'
+    puts '11 - Add a movie'
+    puts '12 - Add a game'
+    puts '13 - Exit'
+    
+    choice = gets.chomp.to_i
+    
+    case choice
+    when 1
+      # run the list of books
+    when 2
+      # run the list of music albums
+    when 3
+      # run the list of movies
+    when 4
+      # run the list of games
+    when 5
+      # run the list of genres
+    when 6
+      # run the list of labels
+    when 7
+      # run the list of authors
+    when 8
+      # run the list of sources
+    when 9
+      # Add a book
+    when 10
+      # Add a music album
+    when 11
+      # Add a movie
+    when 12
+      # Add a game
+    when 13
+      break # Exit the loop when the user chooses 13
+    else
+      puts 'Invalid choice'
     end
+  end
 end
 
-main()
+main
