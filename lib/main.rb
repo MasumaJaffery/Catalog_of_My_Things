f0c2836e5c1351f08b7e5f20f8b7a0dceb0b45ae
Najibullah Jafari <121656832+najibullahjafari@users.noreply.github.com>
2023-09-27 10:47:25 +0430

    update the movie

--- lib/main.rb
+++ lib/main.rb
@@ -1,53 +1,47 @@
-def main
-  puts 'Welcome to our Catalog App'
-  loop do # Fixed the loop do syntax
-    puts '1 - List all books'
-    puts '2 - List all music albums'
-    puts '3 - List all movies'
-    puts '4 - List of games'
-    puts "5 - List all genres (e.g 'Comedy', 'Thriller')"
-    puts "6 - List all labels (e.g. 'Gift', 'New')"
-    puts "7 - List all authors (e.g. 'Stephen King')"
-    puts "8 - List all sources (e.g. 'From a friend', 'Online shop')"
-    puts '9 -  Add a book'
-    puts '10 - Add a music album'
-    puts '11 - Add a movie'
-    puts '12 - Add a game'
-    puts '13 - Exit'
-    
-    choice = gets.chomp.to_i
-    
-    case choice
-    when 1
-      # run the list of books
-    when 2
-      # run the list of music albums
-    when 3
-      # run the list of movies
-    when 4
-      # run the list of games
-    when 5
-      # run the list of genres
-    when 6
-      # run the list of labels
-    when 7
-      # run the list of authors
-    when 8
-      # run the list of sources
-    when 9
-      # Add a book
-    when 10
-      # Add a music album
-    when 11
-      # Add a movie
-    when 12
-      # Add a game
-    when 13
-      break # Exit the loop when the user chooses 13
-    else
-      puts 'Invalid choice'
-    end
+# Define the 'actions' hash
+ACTIONS = {
+  1 => :list_books,
+  2 => :list_music_albums,
+  3 => :list_movies,
+  4 => :list_games,
+  5 => :list_genres,
+  6 => :list_labels,
+  7 => :list_authors,
+  8 => :list_sources,
+  9 => :add_book,
+  10 => :add_music_album,
+  11 => :add_movie,
+  12 => :add_game,
+  13 => :exit_application
+}.freeze
+
+def handle_option(option)
+  action = ACTIONS[option]
+  if action
+    send(action)
+  else
+    puts 'Invalid option.'
   end
+end
+
+def display_menu
+  puts 'Welcome to our Catalog App'
+
+  loop do
+    ACTIONS.each { |k, v| puts "#{k} - #{v.to_s.tr('_', ' ')}" }
+    print 'Option: '
+    choice = gets.chomp.to_i
+    handle_option(choice)
+  end
+end
+
+def exit_application
+  puts 'exited'
+  exit
+end
+
+def main
+  display_menu
 end
 
 main
--- lib/movie.rb
+++ lib/movie.rb
@@ -1,11 +1,35 @@
-require_relative './item'
+require_relative 'item'
 
-class Movie > Item
-    attr_accessor: selint
-    def initialize(selint)
-        @selint = selint
+class Movie < Item
+  attr_reader :silent
+
+  def initialize(silent, label, author, genre, publish_date, id: nil)
+    super(label, author, genre, publish_date)
+    @silent = silent
+    @id = id || Random.rand(1...1000)
+  end
+
+  # Override parent method
+  def can_be_archived?
+    if super || @silent
+      true
+    else
+      false
     end
-
-    def can_be_archived?
-        return true if super(can_be_archived) = true else false
+  end
 end
+
+# Create an instance of the Movie 
+movie = Movie.new(true, "Inception", "Christopher Nolan", "Science Fiction", "2023-09-23")
+puts "Silent: #{movie.silent}"
+puts "Label: #{movie.label}"
+puts "Author: #{movie.author}"
+puts "Genre: #{movie.genre}"
+puts "Publish Date: #{movie.publish_date}"
+puts "ID: #{movie.id}"
+if movie.can_be_archived?
+  puts "This movie can be archived."
+else
+  puts "This movie cannot be archived."
+end
+
