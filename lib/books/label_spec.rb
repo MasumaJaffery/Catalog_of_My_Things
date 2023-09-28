e59ae69c7bd3525e1e5bd0ecaed437fb94b92b99
MasumaJaffery <masumajaffery1@gmail.com>
2023-09-27 17:38:57 -0700

    Finally!

--- lib/books/label_spec.rb
+++ lib/books/label_spec.rb
@@ -1,9 +1,24 @@
+# label_spec.rb
+
 require_relative '../label'
-require 'date'
 
 describe Label do
   let(:label) { Label.new('Rainbow', 'blue', [], '2023-09-27') }
-  
+
+  describe '#initialize' do
+    it 'initializes' do
+      title = 'Rainbow'
+      color = 'blue'
+      publish_date = '2023-09-27'
+
+      # Create a new Label object with the specified attributes
+      label = Label.new(title, color, [], publish_date)
+
+      # Check that the instance variables are correctly initialized
+      expect(label.title).to eq(title)
+    end
+  end
+
   describe '#add_item' do
     it 'appends an item to the items array and sets the label' do
       item = double('item')
