b3831f31163907f10457a9babdfa7c3a12fba729
Grandi0z <josephlkhausi@gmail.com>
2023-09-28 14:15:51 +0200

    Creation of a schema.sql

---
+++ lib/games/sql/schema.sql
@@ -0,0 +1,28 @@
+CREATE DATABASE catalog_of_my_things
+
+CREATE TABLE Game (
+    id INT GENERATED ALWAYS AS IDENTITY,
+    title VARCHAR(30) NOT NULL,
+    publish_date DATE NOT NULL,
+    multiplayer BOOLEAN NOT NULL,
+    last_played_at DATE NOT NULL,
+    genre_id INT DEFAULT NULL,
+    source_id INT DEFAULT NULL,
+    label_id INT DEFAULT NULL,
+    PRIMARY KEY(id)
+);
+
+CREATE TABLE Creation(
+    item_id INT,
+    author_id INT,
+    PRIMARY KEY(item_id, author)
+    CONSTRAINT FK_author FOREIGN KEY (author_id) REFERENCES Author (id)
+    CONSTRAINT FK_item FOREIGN KEY (item_id) REFERENCES Game (id)
+);
+
+CREATE TABLE Author (
+    id INT GENERATED ALWAYS AS IDENTITY,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    PRIMARY KEY(id)
+);
