019913d5d52eabe21fc805a7e7edbdb1bee88342
Glauber <glauber.bannwart@gmail.com>
2023-09-28 12:16:20 -0300

    add console options for music album and genre

--- lib/app.rb
+++ lib/app.rb
@@ -2,13 +2,18 @@ require_relative 'item'
 require_relative 'movie'
 require_relative 'source'
 require_relative 'saveData'
+require_relative 'genre'
+require_relative 'musicalbum'
 
 class App
   attr_accessor :movies, :sources
+  attr_reader :music_albums, :genres
 
   def initialize
     @movies = []
     @sources = []
+    @music_albums = []
+    @genres = []
     @savedata = SaveData.new
     load_data
   end
@@ -17,12 +22,58 @@ class App
     @savedata.load_data
     @movies = @savedata.movies
     @sources = @savedata.sources
+    @music_albums = @savedata.music_albums
+    @genres = @savedata.genres
   end
 
   def save_data
     @savedata.movies = @movies
     @savedata.sources = @sources
     @savedata.save_data
+    @savedata.genres = @genres
+    @savedata.save_data
+  end
+
+  def list_music_albums
+    if @music_albums.empty?
+      puts 'No music albums available.'
+    else
+      @music_albums.each_with_index do |album, index|
+        puts "#{index + 1}. #{album.label} by #{album.author} (#{album.genre.name})"
+      end
+    end
+  end
+
+  def list_genres
+    if @genres.empty?
+      puts 'No genres available.'
+    else
+      @genres.each_with_index do |genre, index|
+        puts "#{index + 1}. #{genre.name}"
+      end
+    end
+  end
+
+  def add_music_album
+    puts 'Enter album label:'
+    label = gets.chomp
+    puts 'Enter album author:'
+    author = gets.chomp
+    puts 'Enter album genre:'
+    genre_name = gets.chomp
+    genre = @genres.find { |g| g.name == genre_name } || Genre.new(genre_name)
+    @genres << genre unless @genres.include?(genre)
+    puts 'Enter publish date (yyyy-mm-dd):'
+    publish_date = gets.chomp
+    puts 'Available on Spotify? (y/n):'
+    on_spotify = gets.chomp.downcase == 'y'
+
+    album = MusicAlbum.new(label, author, genre, publish_date, on_spotify)
+    @music_albums << album
+    genre.add_item(album)
+
+    puts 'Album added successfully!'
+    save_data
   end
 
   def list_movies
--- lib/savedata.rb
+++ lib/savedata.rb
@@ -1,26 +1,71 @@
 require 'json'
 
 class SaveData
-  attr_accessor :sources, :movies
+  attr_accessor :sources, :movies, :music_albums, :genres
 
   def initialize
     @sources = []
     @movies = []
+    @genres = []
+    @music_albums = []
   end
 
   # Load data from JSON files
   def load_data
     load_movies
     load_source
+    load_genres
+    load_music_albums
   end
 
   # Save data to JSON files
   def save_data
     save_movies
     save_source
+    save_genres
+    save_music_albums
     puts 'Data saved successfully.'
   rescue StandardError => e
     puts "Error saving data: #{e.message}"
+  end
+
+  def load_genres
+    return unless File.exist?('./json/genres.json')
+
+    json_str = File.read('./json/genres.json')
+    @genres = JSON.parse(json_str).map do |genre_data|
+      Genre.new(genre_data['name'])
+    end
+  end
+
+  def save_genres
+    File.open('./json/genres.json', 'w') do |file|
+      file.puts @genres.map { |genre| { 'name' => genre.name } }.to_json
+    end
+  end
+
+  def load_music_albums
+    return unless File.exist?('./json/music_albums.json')
+
+    json_str = File.read('./json/music_albums.json')
+    @music_albums = JSON.parse(json_str).map do |album_data|
+      genre = @genres.find { |g| g.name == album_data['genre'] }
+      MusicAlbum.new(album_data['label'], album_data['author'], genre, album_data['publish_date'], album_data['on_spotify'])
+    end
+  end
+
+  def save_music_albums
+    File.open('./json/music_albums.json', 'w') do |file|
+      file.puts JSON.pretty_generate(@music_albums.map do |album|
+        {
+          'label' => album.label,
+          'author' => album.author,
+          'genre' => album.genre.name, # Assume genre is not nil
+          'publish_date' => album.publish_date.to_s, # Convert Date to string
+          'on_spotify' => album.on_spotify
+        }
+      end)
+    end
   end
 
   def load_movies
