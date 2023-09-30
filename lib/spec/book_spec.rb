17c3177806f29838243324e74e4bbea522968179
Najibullah Jafari <121656832+najibullahjafari@users.noreply.github.com>
2023-09-30 09:03:48 +0430

    Update book_spec.rb

--- lib/spec/book_spec.rb
+++ lib/spec/book_spec.rb
@@ -2,34 +2,19 @@ require_relative '../item'
 require_relative '../book'
 
 describe Book do
-  let(:book) { Book.new('Imam Ali a.s', 'good', '2001-01-01') }
-
-  describe '#initialize' do
-    it 'initializes' do
-      publisher = 'Imam Ali a.s'
-      cover_state = 'good'
-      publish_date = '2001-01-01'
-
-      # Create a new Book object with the specified attributes
-      book = Book.new(publisher, cover_state, publish_date)
-
-      # Check that the instance variables are correctly initialized
-      expect(book.cover_state).to eq(cover_state)
-    end
-  end
+  let(:book) { Book.new('Imam Ali a.s', 'Nahjul-e-Balgah', '2001-01-01', false) }
 
   describe '#can_be_archived?' do
     context 'when cover_state is "bad"' do
       it 'returns true' do
-        book = Book.new('Imam Ali a.s', 'bad', '2001-01-01')
+        book.cover_state = 'bad'
         expect(book.can_be_archived?).to be(true)
       end
     end
 
-    context 'when cover_state is "good"' do
-      it 'returns false' do
-        book = Book.new('Imam Ali a.s', 'good', '2001-01-01')
-        expect(book.can_be_archived?).to be(false)
+    context 'when test cover state"' do
+      it 'should returns expected info' do
+        expect(book.cover_state).to eq ('Nahjul-e-Balgah')
       end
     end
   end
